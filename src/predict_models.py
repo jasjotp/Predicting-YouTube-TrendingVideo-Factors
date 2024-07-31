1807902601cbd0d74b7eacbf2206c9d4680070de
Aiden R Carelse <aidenrcarelse@gmail.com>
2024-07-30 17:04:42 -0700

    updated predictions

--- src/predict_models.py
+++ src/predict_models.py
@@ -1,76 +1,139 @@
 import pandas as pd
-from sklearn.ensemble import VotingClassifier
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import make_pipeline
 import os
 
-# OLS, PCA. Randm forest classification/regression to classify. SVM. KNN
+# All the numerical data columns
+x_columns = ['times_trending', 'tag_count', 'likes', 'views', 'dislikes', 'comment_count', 'category_id',
+             'description_length', 'year']
+
+
 # Get the list of tags
 def split_tags(tags):
     tags_list = tags.split('|')
     return [tag.strip().lower() for tag in tags_list]
 
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-old_data = pd.read_csv(os.path.join(current_dir, '..', 'data', 'CAvideos.csv'), parse_dates=['publish_time'])
-new_data = pd.read_csv(os.path.join(current_dir, '..', 'data', 'CAvideos2020.csv'), parse_dates=['publishedAt'])
+def main():
+    # Parse the data into frames
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    old_data = pd.read_csv(os.path.join(current_dir, '..', 'data', 'CAvideos.csv'), parse_dates=['publish_time'])
+    new_data = pd.read_csv(os.path.join(current_dir, '..', 'data', 'CAvideos_2020-2024.csv'), parse_dates=['publishedAt'])
 
-old_data['year'] = old_data['publish_time'].dt.year
-new_data['year'] = new_data['publishedAt'].dt.year
-old_data = old_data[old_data['year'] >= 2017]
+    # Clean the data
+    data = combine_data(old_data, new_data)
+    data = add_data_columns(data)
+    data = filter_data(data)
 
-old_data = old_data[old_data['comments_disabled'] == False]
-new_data = new_data[new_data['comments_disabled'] == False]
-old_data = old_data[old_data['ratings_disabled'] == False]
-new_data = new_data[new_data['ratings_disabled'] == False]
+    # Predictions
+    predict_columns(data)
+    predict_mock_data(data)
 
-old_data = old_data.dropna(subset=['views'])
-new_data = new_data.dropna(subset=['view_count'])
 
-old_data['times_trending'] = old_data['video_id'].map(old_data['video_id'].value_counts())
-new_data['times_trending'] = new_data['video_id'].map(new_data['video_id'].value_counts())
-old_data['tag_count'] = old_data['tags'].apply(split_tags).apply(len)
-new_data['tag_count'] = new_data['tags'].apply(split_tags).apply(len)
+# Combine the old and new datasets into one
+def combine_data(old_data, new_data):
+    # Remove columns that don't appear in both sets
+    old_data = old_data.drop('video_error_or_removed', axis=1)
+    new_data = new_data.drop('channelId', axis=1)
 
-# Couldn't use view counts for some reason (Nan values)
-old_data2 = pd.DataFrame(old_data, columns=['times_trending', 'tag_count', 'dislikes', 'comment_count'])
-new_data2 = pd.DataFrame(new_data, columns=['times_trending', 'tag_count', 'likes', 'dislikes', 'comment_count'])
+    # Rename the new columns to match the old ones
+    new_data.columns = ['video_id', 'title', 'publish_time', 'channel_title', 'category_id', 'trending_date', 'tags',
+                        'views', 'likes', 'dislikes', 'comment_count', 'thumbnail_link', 'comments_disabled',
+                        'ratings_disabled', 'description']
 
-X = pd.concat([old_data2, new_data2])
-y = pd.concat([old_data['year'], new_data['year']])
+    # Return the concatenated data
+    return pd.concat([old_data, new_data], ignore_index=True)
 
-nan_indices = X[X.isna().any(axis=1)].index
-X = X.drop(nan_indices)
-y = y.drop(nan_indices)
 
-X = X.values
-y = y.values
+# Add new insightful columns to the data frame
+def add_data_columns(data):
+    # Store the year value separately
+    data['year'] = data['publish_time'].dt.year
 
-X_train, X_valid, y_train, y_valid = train_test_split(X, y)
+    # Track how many times each video has trended
+    data['times_trending'] = data['video_id'].map(data['video_id'].value_counts())
 
-# SVC was taking too long so I removed it
-model = VotingClassifier([
-    ('nb', GaussianNB()),
-    ('knn', KNeighborsClassifier(10)),
-    ('tree1', DecisionTreeClassifier(max_depth=100)),
-    ('tree2', DecisionTreeClassifier(min_samples_leaf=20)),
-])
-model.fit(X_train, y_train)
-print('VotingClassifier score on predicting years:', model.score(X_valid, y_valid))
+    # Add a tag count column
+    data['tag_count'] = data['tags'].apply(split_tags).apply(len)
 
-X2 = pd.concat([old_data2, new_data2])
-y2 = pd.concat([old_data['category_id'], new_data['categoryId']])
+    # Count the length of the video's description in characters
+    data['description_length'] = data['description'].apply(lambda x: len((str(x)).split()))
 
-X2 = X2.drop(nan_indices)
-y2 = y2.drop(nan_indices)
+    return data
 
-X2 = X2.values
-y2 = y2.values
 
-X_train2, X_valid2, y_train2, y_valid2 = train_test_split(X2, y2)
+# Filter and clean the data of unwanted rows
+def filter_data(data):
+    # There is not enough data before 2017 to work with
+    data = data[data['year'] >= 2017]
 
-model.fit(X_train2, y_train2)
-print('VotingClassifier score on predicting category ID:', model.score(X_valid2, y_valid2))
+    # Remove rows where some values are hidden
+    data = data[data['comments_disabled'] != True]
+    data = data[data['ratings_disabled'] != True]
+
+    # The view values can be too large to work with, so we split into hundreds of thousands
+    data['views'] = (data['views'] / 100000).astype(int)
+
+    return data
+
+
+# Predict and score the accuracy of certain columns
+def predict_columns(data):
+    prediction_columns = ['year', 'times_trending', 'views']
+
+    # Loop through all the columns we want to predict
+    for y_column in prediction_columns:
+        # Remove the label form the x values
+        new_x_columns = x_columns[:]
+        new_x_columns.remove(y_column)
+
+        # Split the data into train and validate sets
+        if y_column == 'views':
+            # Views can run into memory issues, so a subsample of the dataset is needed
+            X_train, X_valid, y_train, y_valid = get_data_splits(data.sample(n=100000), new_x_columns, y_column)
+        else:
+            X_train, X_valid, y_train, y_valid = get_data_splits(data, new_x_columns, y_column)
+
+        # Create a random forest classifier model with a scaler
+        model = make_pipeline(MinMaxScaler(), RandomForestClassifier(n_estimators=50))
+
+        # Train model and print score
+        model.fit(X_train, y_train)
+        print('VotingClassifier score on predicting', y_column, ':', model.score(X_valid, y_valid))
+
+
+# Predict how many times a video with generated data will trend
+def predict_mock_data(data):
+    # Predict for times_trending
+    new_x_columns = x_columns[:]
+    new_x_columns.remove('times_trending')
+
+    # Split the data into train and validate sets
+    X_train, X_valid, y_train, y_valid = get_data_splits(data, new_x_columns, 'times_trending')
+    model = make_pipeline(MinMaxScaler(), RandomForestClassifier(n_estimators=50))
+
+    # Generate mock data
+    mock_data = np.mean(X_train, axis=0).reshape(1, -1)
+
+    # Train model and print prediction
+    model.fit(X_train, y_train)
+    print('Prediction on how many times the mock data will trend:', model.predict(mock_data))
+
+
+# Split the data into train and validate sets
+def get_data_splits(data, new_x_columns, y_column):
+    # Sort X and y values
+    X = pd.DataFrame(data, columns=new_x_columns)
+    y = data[y_column]
+
+    # Turn the pandas frames into numpy arrays
+    X = X.values
+    y = y.values
+
+    return train_test_split(X, y)
+
+
+main()
